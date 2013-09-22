5f30471268c68ed982e9665e2ff7561b16f339b0
dbcapprentice <dbcapprentice@dbc.com>
2013-09-22 16:44:53 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hartl::Application.config.secret_token = '1002771a41afc2ffe324e832075469bdb14fcf431406541727aa8987d2f8f37022bf4eede0772cba105068b093744f1da1375a682c20c7620fba535dd6e9f7d6'
